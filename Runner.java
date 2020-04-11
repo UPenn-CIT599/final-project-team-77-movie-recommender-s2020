9618757f761a1d6442f926d98281c836b106c205
vivalavyvy <63222780+vivalavyvy@users.noreply.github.com>
2020-04-10 20:30:46 -0500

    Added Javadoc comments, Prompted User Inputs

--- Runner.java
+++ Runner.java
@@ -1,35 +1,50 @@
+/**
+ * @Author: Team 77 (Manuel Moreno, Vy Nguyen, Musen Wen)
+ * Date: 04/10/2020
+ * Class: MCIT Spring 2020
+ */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Runner {
 	public static void main(String[] args) {
-		//Intro text and questions
-		System.out.println("Hello! Welcome to movie recommender. \nWe have over 80,000 movies waiting for you. "
-				+ "\n\nPlease answer the following questions so that we may give you the correct recommendations.If you"
-				+ "feel indefferent towards any of the following questions please enter a 0.");
+		/** 
+		* This is the main method of the Movie Recommender program.
+		* In this method, we prompt the users for inputs such as genre, year, rating, actors, director, and number of movie recommendations.
+		* Based on the inputs, we call the Compare Input function from the Recommender Class to receive an array of suitable movies.
+		* After generating a list of suitable movies, we call the RandomPull function to return a random top 5 movies from the suitable movie list. 
+		* At the end, we print a list of 5 movie recommendations to the user. 
+		*/
+		//Intro text and questions. We want inputs from users, specifically genre, year, rating, actors, director, and number of movie recommendations.
+		System.out.println("Hello! Welcome to our Movie Recommendation System. \nWe have over 80,000 movies waiting for you. "
+				+ "\n\nPlease answer the following questions so that we may customize our recommendations. If you"
+				+ "feel indifferent towards any of the following questions, please enter 0.");
 		Scanner userInputScanner = new Scanner(System.in);
-		System.out.println("What genre are you looking for?");
+
+		System.out.println("Which genre do you prefer?");
 		String genre = userInputScanner.nextLine();
-		System.out.println("What year range would you like? \nSelect a number: \n1. 1900-1929 \n2. 1930-1959 \n3. 1960-1979"
+		System.out.println("Which year range would you like? \nSelect a number: \n1. 1900-1929 \n2. 1930-1959 \n3. 1960-1979"
 				+ "\n4. 1980-1999 \n5. 2000-2019");
 		int year = Integer.parseInt(userInputScanner.nextLine());
-		System.out.println("In terms of movie rating (from 1-10), above what rating would you like your movie recommendations?");
+		System.out.println("In terms of movie rating (from 1-10), what is the minimum rating your movies should have?");
 		double rating = Integer.parseInt(userInputScanner.nextLine());
-		System.out.println("Would you like to see any actor or actress in the film? (Please pick and spell a single name correctly)");
+		System.out.println("Which actor or actress would you like the movie cast to have, if any? Please write his/her name in this format, (Firstname Lastname)");
 		String actors = userInputScanner.nextLine();
-		System.out.println("Would you like the film to be directed by a specific person? (Please spell their name correctly)");
+		System.out.println("Which director would you like the movie to have? Please write his/her name in this format, (Firstname Lastname)");
 		String director = userInputScanner.nextLine();
-		System.out.println("How many movies would you like recommended? (We will only show relevant movies)");
+		System.out.println("How many movie recommendations would you like to see? The default is 5 movie recommendations.");
 		int numberOfMovies = Integer.parseInt(userInputScanner.nextLine());
-		System.out.println("Thank you for your input! Gives us a minute...");
-		//Comparing the input with the movie database
+		System.out.println("Thank you for your input! Your recommendations are loading...");
+		
+		//Comparing the input with the movie database. Disregarding other variables.
 		Recommender recommend = new Recommender();
 		ArrayList<Movie> movieRecommendations = recommend.CompareInput(genre, year, rating, actors, director);
-		//Picking five random movies
-		movieRecommendations = recommend.RandomPull(numberOfMovies, movieRecommendations);
-		//Printing out the recommendations
-		for(int i = 0; i < movieRecommendations.size(); i++) {
+		//Picking five random movies by default. Number of movie recommendations is dependent on numberOfMovies input. Can be more or less than 5.
+		ArrayList<Movie>Top5movieRecommendations = recommend.RandomPull(numberOfMovies, movieRecommendations);
+		//Printing out the recommendations. Output is Movie Title. Default is a list of 5 movies.
+		for(int i = 0; i < Top5movieRecommendations.size(); i++) {
 			System.out.println(i+1 + ". " + movieRecommendations.get(i).getMovieTitle());
 		}
 	}	
