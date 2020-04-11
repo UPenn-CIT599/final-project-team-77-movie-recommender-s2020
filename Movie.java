2119a03650d67856ab77c724216a4a54faf8f481
vivalavyvy <63222780+vivalavyvy@users.noreply.github.com>
2020-04-11 00:10:01 -0500

    Added JavaDoc Comments. Working in Eclipse.

--- Movie.java
+++ Movie.java
@@ -1,3 +1,9 @@
+  
+/**
+ * @Author: Team 77 (Manuel Moreno, Vy Nguyen, Musen Wen)
+ * Date: 04/10/2020
+ * Class: MCIT Spring 2020
+ */
 
 public class Movie {
 	private String movieTitle; 
@@ -6,7 +12,18 @@ public class Movie {
 	private int movieYear;
 	private String director;
 	private String actors;
-	
+	/**
+	 * 
+	 * @param movieTitle
+	 * @param movieRating
+	 * @param genre
+	 * @param movieYear
+	 * @param director
+	 * @param actors
+	 * 
+	 * Movie class has a constructor which defines movieTitle, movieRating, genre, movieYear, director, actors
+	 * we also created getter functions to return each parameter.
+	 */
 	public Movie(String movieTitle, double movieRating, String genre, int movieYear,
 			String director, String actors) {
 		this.movieTitle = movieTitle;
